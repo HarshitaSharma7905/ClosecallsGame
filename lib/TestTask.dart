e191e1d88d23fd4867c531474bebec86d83e806c
Harshita sharma <harshita7905113683@gmail.com>
2023-07-01 11:10:10 +0530

    few changes

--- lib/TestTask.dart
+++ lib/TestTask.dart
@@ -180,3 +180,21 @@
 //     );
 //   }
 // }
+import 'package:flutter/material.dart';
+
+class Task extends StatefulWidget {
+  const Task({Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('hello'),
+    );
+  }
+}
+
